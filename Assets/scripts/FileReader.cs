a07ac49a7a9efb924facab6ec92356d7de6812d5
Alexander Solan <alexander.solan@gmail.com>
2015-10-30 15:44:21 +0200

    Basic level generation.

---
+++ Assets/scripts/FileReader.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+using System;
+
+public class FileReader : MonoBehaviour {
+
+	private static FileInfo sourceFile = null;
+	private static StreamReader reader = null;
+	private static string text = " ";
+
+	public static int[,] readFile(string s) {
+
+		int length = 0;
+		int width = 0;
+		sourceFile = new FileInfo(s);
+		reader = sourceFile.OpenText ();
+
+		while (true) {
+			text = reader.ReadLine();
+			if (text != null) {
+				int ticker = 0;
+
+				for (int i = 1; i <= text.Length; i++) {
+
+					if (text.Substring(i-1, 1) == ",") {
+						ticker++;
+					} else if (text.Substring(i-1,1) == ";") {
+						length++;
+						ticker++;
+					}
+				}
+				if (ticker > width) {
+
+					width = ticker;
+
+				}
+
+			} else {
+				break;
+			}
+		}
+		int[,] grid = new int[length,width];
+
+		reader.Close();
+
+		sourceFile = new FileInfo(s);
+
+		reader = sourceFile.OpenText ();
+
+		for (int i = 0; i < length; i++) {
+
+			text = reader.ReadLine();
+
+			if (text != null) {
+
+				int ticker = 0;
+				int prevStop = 0;
+
+				for (int k = 1; k <= text.Length; k++) {
+
+					if (ticker == width) {
+						continue;
+					}
+
+					if (text.Substring(k-1, 1) == "," || text.Substring(k-1, 1) == ";") {
+						int number;
+						bool success = Int32.TryParse((string)text.Substring(prevStop, 1), out number);
+						if (success) {
+							grid[i,ticker] = number;
+						} else {
+							grid[i,ticker] = 0;
+						}
+						prevStop = k;
+						ticker++;
+					}
+				}
+			}
+
+		}
+
+		reader.Close();
+
+		return grid;
+
+	}
+
+}
--- Assets/scripts/GCScript.cs
+++ Assets/scripts/GCScript.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class GCScript : MonoBehaviour {
+
+	public static float xShift = 1.618f, yShift = 0.4f, zShift = 2.618f;
+	public GameObject slab;
+	public Material plainWhite, plainLightGrey;
+	public Camera mainCamera;
 
 	// Use this for initialization
 	void Start () {
-	
+
+		int[,] grid = FileReader.readFile("./assets/levels/level01.lvl");
+		int i = 0;
+		StartCoroutine(CreateDelay(i, grid));
+
+		mainCamera.transform.position = new Vector3(grid.GetLength (1)/2 * 1.618f, mainCamera.transform.position.y, mainCamera.transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	IEnumerator CreateDelay(int i, int[,] grid) {
+		yield return new WaitForSeconds(0.02f);
+		generateRow(i, grid);
+	}
+
+	void generateRow(int i, int[,] grid) {
+		for (int j = 0; j < grid.GetLength (1); j++) {
+			
+			if (grid[i,j] != 0) {
+				
+				GameObject newSlab = Instantiate (slab, new Vector3(j*xShift, 0, i*zShift), Quaternion.identity) as GameObject;
+				
+				if ((i+j) % 2 == 0) {
+					newSlab.gameObject.GetComponent<Renderer>().material = plainWhite;
+				} else {
+					newSlab.gameObject.GetComponent<Renderer>().material = plainLightGrey;
+				}
+			}
+		}
+		if (i < grid.GetLength(0)) {
+			i++;
+			StartCoroutine(CreateDelay(i, grid));
+		}
 	}
 }
