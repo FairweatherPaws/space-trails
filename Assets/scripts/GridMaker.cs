1907621ca6241066ecab957e76578f1f9ac30bd1
Petri Autio <petri.mikael.autio@gmail.com>
2015-11-08 00:36:40 +0000

    Progress on grid creator.

--- Assets/scripts/GridMaker.cs
+++ Assets/scripts/GridMaker.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.IO;
+using System;
 
 public class GridMaker : MonoBehaviour {
 
@@ -21,7 +24,7 @@ public class GridMaker : MonoBehaviour {
 
         if (stringList != null)
         {
-         
+
             for (int l = 0; l < length; l++)
             {
                 int ticker = 0;
@@ -45,43 +48,50 @@ public class GridMaker : MonoBehaviour {
         string[,] grid = new string[length, width];
 
         // Fill grid
-        /*
-		for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
 
-			if (text != null) {
+            int ticker = 0;
+            int prevStop = 0;
 
-				int ticker = 0;
-				int prevStop = 0;
+            for (int k = 1; k <= stringList[i].Length; k++)
+            {
 
-				for (int k = 1; k <= text.Length; k++) {
+                if (ticker == width)
+                {
+                    continue;
+                }
 
-					if (ticker == width) {
-						continue;
-					}
+                if (stringList[i].Substring(k - 1, 1) == "," || stringList[i].Substring(k - 1, 1) == ";")
+                {
 
-					if (text.Substring(k-1, 1) == "," || text.Substring(k-1, 1) == ";") {
-						/*
-						int number;
-						bool success = Int32.TryParse((string)text.Substring(prevStop, 1), out number);
-						if (success) {
-							grid[i,ticker] = number;
-						} else {
-							grid[i,ticker] = 0;
-						}
-						
-						if (k - prevStop > 0) {
-							grid[i,ticker] = text.Substring(prevStop,k-prevStop);
-						} else {
-							grid[i,ticker] = "0";
-						}
+                    int number;
+                    bool success = Int32.TryParse((string)stringList[i].Substring(prevStop, 1), out number);
+                    if (success)
+                    {
+                        grid[i, ticker] = number.ToString();
+                    }
+                    else
+                    {
+                        grid[i, ticker] = "0";
+                    }
 
-						prevStop = k;
-						ticker++;
-					}
-				}
-			}*/
+                    if (k - prevStop > 0)
+                    {
+                        grid[i, ticker] = stringList[i].Substring(prevStop, k - prevStop);
+                    }
+                    else
+                    {
+                        grid[i, ticker] = "0";
+                    }
 
+                    prevStop = k;
+                    ticker++;
+                }
+
+            }
+           
+        }
+        Debug.Log(grid);
     }
-
-
 }
